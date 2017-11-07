efe3a9c554392f2b10282b12a10a9d3f2f2ab7c9
Denys Batrak <baden.i.ua@gmail.com>
2017-11-07 23:37:27 +0200

    Deploy for testing on other targets.

---
+++ README.md
@@ -0,0 +1,26 @@
+# Experiments with https://github.com/PolymerLabs/lit-html
+
+## Prepare
+
+```
+  npm i
+```
+
+## Develop
+
+```
+  npm run start:dev
+```
+
+## Build
+```
+  npm run build
+```
+
+## Deploy
+
+```
+  npm run deploy
+```
+
+Open link: https://baden.github.io/lit-html-testing/
--- src/app.js
+++ src/app.js
@@ -1,24 +1,40 @@
-import { html, render } from 'lit-html/lib/lit-extended';
+import { html, directive, render } from 'lit-html/lib/lit-extended';
+// import { directive } from 'lit-html';
+
+console.log("directive=", directive);
+
+// const myComponent = (f) => directive((part) => {
+//   return f();
+// });
+
+const title = 'title';
+const header = html`<h1>${title}</h1>`;
 
 export class App {
-  get name() { return this._name;}
-  set name(v) { this._name = v; this.invalidate(); }
+  get counter() { return this._counter || 0;}
+  set counter(v) { this._counter = v; this.invalidate(); }
   
   constructor(parent) {
     this.parent = parent;
-    this.name = 'world';
+    this.counter = 0;
   }
   
-  _click(e) {
-    console.log('App:click', e);
-    this.name = 'whole universe';
+  _inc(e) {
+    console.log('App:inc', e);
+    this.counter++;
+  }
+
+  _dec(e) {
+    console.log('App:dec', e);
+    this.counter--;
   }
   
   render() {
     return html`
-      <button class="greater" on-click="${e => this._click(e)}">
-        Hello, ${this.name}!
-      </button>
+      ${header}
+      <button on-click="${e => this._dec(e)}">-</button>
+      ${this.counter}
+      <button on-click="${e => this._inc(e)}">+</button>
     `;
   }
   
